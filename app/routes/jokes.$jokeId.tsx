59f05a779e67ebb145f12cacd692a98d0afe0823
Coolfeather2 <coolfeather.o@gmail.com>
2023-06-07 14:33:16 +0800

    Database

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,11 +1,28 @@
+
+import { db } from "~/utils/db.server";
+
+import { LoaderArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ params }: LoaderArgs) {
+  const joke = await db.joke.findUnique({
+    where: {id: params.jokeId}
+  });
+
+  if (!joke) {
+    throw new Error("Joke not found")
+  };
+  
+  return json({joke});
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<typeof loader>();
+
     return (
       <div>
         <p>Here's your hilarious joke:</p>
-        <p>
-          Why don't you find hippopotamuses hiding in trees?
-          They're really good at it.
-        </p>
+        <p>{data.joke.content}</p>
       </div>
     );
   }
